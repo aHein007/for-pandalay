4e946fa123779ba398a1c327c0ffeb264613b4aa
aHein007 <ahein8604@gmail.com>
2023-08-16 18:16:44 +0630

    need to fix header nav scroll effect

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+   
   });
   var swiper2 = new Swiper(".bg-slider", {
     loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    spaceBetween: 0,
     thumbs: {
       swiper: swiper,
     },
   });
+
+  // navigation bar effects
+
+  window.addEventListener("scroll",function(){
+    const header =this.document.querySelector("header");
+    header.classList.toggle("sticky",this.window.scrollY > 0) // this code is when i scroll down , to make class "sticky"
+  })
